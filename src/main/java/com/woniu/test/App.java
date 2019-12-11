0d0eadbc11bff8f960b40c4e5db78b653021d974
sss <sss@163.com>
2019-12-11 08:44:42 +0800

    commit

--- src/main/java/com/woniu/test/App.java
+++ src/main/java/com/woniu/test/App.java
@@ -3,6 +3,7 @@ package com.woniu.test;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("right!!!");
-		System.out.println("left!!!");	
+		System.out.println("left!!!");
+		System.out.println("hot fix...");
 	}
 }
